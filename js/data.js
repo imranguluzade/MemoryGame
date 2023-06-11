4e0dca679cb5128be6fa61cc0ef3fdee8cd42ec6
ImranGuluzada <imranguluzadeh@gmail.com>
2023-06-11 23:21:39 +0400

    dd

---
+++ js/data.js
@@ -0,0 +1,34 @@
+export const marvelCharacters = [
+  {
+    img: "https://www.liveabout.com/thmb/1gDTjsiLZELwHPyZSChA4RlKAHg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/FILM_ironman4-56a834265f9b58b7d0f184d1.jpg",
+    name: "Iron Man",
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/9/90/Spiderman.JPG",
+    name: "Spider-Man",
+  },
+  {
+    img: "https://cf-images.us-east-1.prod.boltdns.net/v1/static/5359769168001/0a823cb0-01a9-4835-a348-c64187783ccb/d37cb96c-805c-4aa2-9f2f-e62d9eb814c7/1280x720/match/image.jpg",
+    name: "Captain America",
+  },
+  {
+    img: "https://www.nme.com/wp-content/uploads/2018/02/Black-Widow-Avengers.jpg",
+    name: "Black Widow",
+  },
+  {
+    img: "https://ichef.bbci.co.uk/images/ic/1200x675/p09t1hg0.jpg",
+    name: "Thor",
+  },
+  {
+    img: "https://www.looper.com/img/gallery/our-hulk-expert-answers-the-worlds-most-difficult-hulk-questions/l-intro-1611178512.jpg",
+    name: "Hulk",
+  },
+  {
+    img: "https://www.troyes-obs.fr/wp-content/uploads/2021/07/Black-Panther-2-sera-le-film-le-plus-special-de-758x569.jpg",
+    name: "Black Panther",
+  },
+  {
+    img: "https://assets-prd.ignimgs.com/2022/07/19/captainmarvel1280-1543892883252-1658264912556.jpg",
+    name: "Captain Marvel",
+  },
+];
---
+++ js/script.js
@@ -0,0 +1,8 @@
+import { marvelCharacters } from "./data.js";
+const lives = document.querySelector(".lives");
+const game = document.querySelector(".game");
+
+let LIVE = 5;
+
+lives.innerHTML = LIVE;
+
